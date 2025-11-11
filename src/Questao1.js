30664a8afc2ec00c286fa2668eaa29182e8bc933
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2025-11-11 14:31:23 -0300

    Update Questao1.js

--- src/Questao1.js
+++ src/Questao1.js
@@ -1,4 +1,4 @@
-// Converta o seguinte objeto JavaScript para uma string JSON e depois recupere-o:
+// 1. Converta o seguinte objeto JavaScript para uma string JSON e depois recupere-o:
 
 
 const usuario = {
