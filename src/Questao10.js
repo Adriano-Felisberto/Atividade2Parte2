fa1d1269dc6be257e83ce6127f20dda318fb9b05
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2025-11-11 14:32:17 -0300

    Fix comment formatting in Questao10.js

--- src/Questao10.js
+++ src/Questao10.js
@@ -1,4 +1,4 @@
-//10. Validação e Tratamento de Erros: Escreva uma função segura que tente
+// 10. Validação e Tratamento de Erros: Escreva uma função segura que tente
 // converter uma string para JSON e trate possíveis erros:
 function parseJSONSafe(jsonString) {
     try {
