a72e3f8d77183c56b8970e4ae9e8eab7d84d31dd
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2025-11-11 14:33:48 -0300

    Update Questao4.js

--- src/Questao4.js
+++ src/Questao4.js
@@ -1,11 +1,13 @@
 // 4. Escreva uma função que formate um objeto de endereço em uma string legível
 // usando template strings:
+
 const endereco = {
 rua: "Av. Paulista",
 numero: 1000,
 cidade: "São Paulo",
 cep: "01310-100"
 };
+
 // Resultado esperado: "Av. Paulista, 1000 - São Paulo - CEP: 01310-100"
 
 let formatadorender = (endereco) => {
